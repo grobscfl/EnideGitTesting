18bba371f28715c5557634ee3962e0154ee07a86
Garrett <g.robberson89@gmail.com>
2016-11-03 10:33:58 -0500

    Branch Testing Push

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	console.log("Hello World!!!!!")
+	console.log("Another Change for a branch test")
   res.render('index', { title: 'Express' });
 });
 
