57fc733d71cabc1401cfaee9e1fd72b0af0a3147
Garrett <g.robberson89@gmail.com>
2016-11-03 10:29:24 -0500

    Second Commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	console.log("hello")
+	console.log("Hello World!!!!!")
   res.render('index', { title: 'Express' });
 });
 
